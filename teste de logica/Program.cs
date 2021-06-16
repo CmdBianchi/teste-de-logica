250367b8223440b680171a38e917fce4c158ce2f
marcus bianchi <marcusbianchi92@hotmail.com>
2021-06-16 12:04:48 -0300

    Adicionar arquivos de projeto.

---
+++ teste de logica/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+class MainClass {
+    public static string StringChallenge(string text) {
+
+        Console.WriteLine($"Normal Text: {text}");
+        string textInverted = new string(text.Reverse().ToArray());
+        Console.WriteLine($"Inverted Text: {textInverted}");
+        Console.ReadLine();
+        return textInverted;
+    }
+    static void Main() {
+        string text = Console.ReadLine();
+        Console.WriteLine(StringChallenge(text));
+    }
+}
